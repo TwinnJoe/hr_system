98919fd17486694d391d134daf9b4add215a2a37
Twinn Joe <sinovuyojoe@gmail.com>
2025-06-24 09:39:25 +0200

    Fix login API URL

--- src/components/Admin.vue
+++ src/components/Admin.vue
@@ -72,7 +72,7 @@ export default {
     },
     async validateLogin() {
       try {
-        const response = await axios.post("http://localhost:3000/api/admin/login", {
+        const response = await axios.post(`${API}/api/admin/login`, {
           username: this.username,
           password: this.password,
         });
