13182616a2c5edf82d758940dbbb69f46bd2e871
Twinn Joe <90099256+TwinnJoe@users.noreply.github.com>
2025-06-30 09:23:03 +0200

    Update Admin.vue

--- src/components/Admin.vue
+++ src/components/Admin.vue
@@ -73,7 +73,7 @@ export default {
     },
     async validateLogin() {
       try {
-        const response = await axios.post(`${API}/api/admin/login`, {
+        const response = await axios.post(`${API}/api/users/login`, {
           username: this.username,
           password: this.password,
         });
