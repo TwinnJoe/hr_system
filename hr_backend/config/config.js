6784fd492682dd92028ad8b1d8c974d4983defda
Twinn Joe <90099256+TwinnJoe@users.noreply.github.com>
2025-08-24 10:56:56 +0200

    Update config.js

--- hr_backend/config/config.js
+++ hr_backend/config/config.js
@@ -1,20 +1,4 @@
 import mysql from "mysql2/promise";
-import dotenv from "dotenv";
-import fs from "fs";
-import path from "path";
-import { fileURLToPath } from "url";
-
-dotenv.config();
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-// Path to the certificate (Windows-compatible)
-const caCertPath = path.join(process.cwd(), "certs", "ca.pem");
-
-if (!fs.existsSync(caCertPath)) {
-  console.error("❌ CA certificate not found at:", caCertPath);
-  process.exit(1);
-}
 
 const pool = mysql.createPool({
   host: process.env.HOST,
@@ -26,7 +10,7 @@ const pool = mysql.createPool({
   connectionLimit: 10,
   ssl: {
     rejectUnauthorized: true,
-    ca: fs.readFileSync(caCertPath), // Load the CA certificate
+    ca: Buffer.from(process.env.DB_CA_CERT, "base64").toString("utf-8")
   },
 });
 
