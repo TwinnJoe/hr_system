44a53716e0940962044189b3975ee7efe7c006f1
Twinn Joe <90099256+TwinnJoe@users.noreply.github.com>
2025-08-25 16:05:11 +0200

    Update app.js

--- hr_backend/app.js
+++ hr_backend/app.js
@@ -4,6 +4,13 @@ import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
 
+import employeeRouter from "./routes/employee.js";
+import attendanceRouter from "./routes/attendance.js";
+import payrollRouter from "./routes/payroll.js";
+import leaveRequestRoutes from "./routes/leaveRequests.js";
+import payslipRouter from "./routes/payslip.js";
+import adminRouter from "./routes/admin.js";
+
 const app = express();
 
 const FRONTEND_URL = process.env.FRONTEND_URL;
