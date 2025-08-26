26c91e93b037a145669342416eb104ae82d5dbb1
Twinn Joe <sinovuyojoe@gmail.com>
2025-08-26 08:51:18 +0200

    name update

--- hr_backend/app.js
+++ hr_backend/app.js
@@ -4,21 +4,21 @@ import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
 
-import employeeRouter from "./routes/employeeRouter.js";
-import attendanceRouter from "./routes/attendanceRouter.js";
-import payrollRouter from "./routes/payrollRouter.js";
-import leaveRequestRoutes from "./routes/leaveRequestRoutes.js";
-import payslipRouter from "./routes/payslipRouter.js";
-import adminRouter from "./routes/adminRouter.js";
+import employeeRouter from "./Routes/employeeRouter.js";
+import attendanceRouter from "./Routes/attendanceRouter.js";
+import payrollRouter from "./Routes/payrollRouter.js";
+import leaveRequestRoutes from "./Routes/leaveRequestRoutes.js";
+import payslipRouter from "./Routes/payslipRouter.js";
+import adminRouter from "./Routes/adminRouter.js";
 
 const app = express();
 
 const FRONTEND_URL = process.env.FRONTEND_URL;
 
 app.use(cors({
-  origin: FRONTEND_URL,      // allow only your frontend
+  origin: FRONTEND_URL,      // allow only my frontend
   methods: ["GET","POST","PUT","DELETE","OPTIONS"],
-  credentials: true           // needed if you use cookies/auth headers
+  credentials: true           // needed if I use cookies/auth headers
 }));
 
 // Handle preflight requests
